b8fc92cc6c9980b4660318b0bf513c3e75d479f5
xitoar <xitoar@hotmail.com>
2022-04-23 12:12:18 -0300

    primer commit| iord changes to the repository

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { InfoComponent } from './info/info.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+
+const routes: Routes = [
+    {path: '', redirectTo: '/info', pathMatch: 'full'},
+    {path: 'info', component:InfoComponent},
+    {path: 'educacion', component:EducacionComponent},
+    {path: 'experiencia', component:ExperienciaComponent},
+    {path: 'proyectos', component:ProyectosComponent},
+    {path: 'contacto', component:ContactoComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ResumenComponent } from './resumen/resumen.component';
+import { MenuComponent } from './menu/menu.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { InfoComponent } from './info/info.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    ResumenComponent,
+    MenuComponent,
+    EducacionComponent,
+    InfoComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    ContactoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
